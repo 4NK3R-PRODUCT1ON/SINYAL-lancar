7bf593865e2581d3a9e4910b595758de229fc294
Anker production <67215583+4NK3R-PRODUCT1ON@users.noreply.github.com>
2020-08-03 10:02:21 +0700

    Add files via upload

---
+++ ping.sh
@@ -0,0 +1,23 @@
+biru='\033[34;1m' #biru
+green='\e[0;32m' #hijau muda
+daun='\033[32;1m' #ijo
+purple='\033[35;1m' #purple
+cyyan='\033[36;1m' #cyan
+merah='\033[31;1m' #merah
+putih='\033[37;1m' #pur
+tai='\033[33;1m' #kuning
+clear
+echo $merah "[]======================[]"
+echo $daun  "    Author 4NK3R   "
+echo $merah "[]======================[]"
+echo 
+echo	
+echo $biru "Masukan Target Ping Contoh"$putih" google.com"
+echo $tai
+read -p "4NK3R >> " ping
+clear
+cowsay -f kiss.cow "4NK3R"
+echo $daun
+ping $ping
+
+
